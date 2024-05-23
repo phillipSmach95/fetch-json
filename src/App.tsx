bc459fff9d2dd3d56fe7707f717fa03c0dacc282
phillipSmach95 <smach.phillip@gmail.com>
2024-05-23 11:05:48 +0200

    fetch types wrong

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState({hits:[]});
+  const [user,setUser]=useState({results:[]});
+  useEffect(()=>{
+    const fetchDataHits = async()=>{
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=react-fetch"
+        );
+
+
+        setData(result.data as typeof data)
+    }
+    fetchDataHits()
+    
+  },[])
+  console.log(data.hits.map((i)=>i.title));
+  
+  const dataHitsMap = data.hits.map((item)=>(
+    <li key={item.objectID}>
+      <a href={item.url} rel="noopener noreferrer">{item.title}</a>
+    </li>
+  ))
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <ul>
+       {dataHitsMap}
+       </ul>
       </header>
     </div>
   );
